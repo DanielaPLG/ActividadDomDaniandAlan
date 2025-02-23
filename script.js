4acdce48999963c8ee1a6804c67b8b1a4fe0cc2e
LAPT-CSH-17 <cshtd.computacion@gmail.com>
2025-02-23 12:54:51 -0600

    Segundo cambio

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 function minimanual(prácticasdeestudio){
     var elem = document.getElementById("contenido");
     }
+    function minimanual(prácticasdeestudio){
+        var elem = document.getElementById("contenido").innerHTML = "este es un manual de prácticas de estudio de informatica para que puedan sobrevivir AJJAAJJ";
+        console.log("minimanual");
+        }
+    minimanual();
+    
+        let div = document.createElement('div');
+        
+        let ul = document.createElement('ul');
+    
+        let items = [
+            'No dejar las tareas para última hora',
+            'No te duermas en clases',
+            'Si no entendiste algo, pídele ayuda al teacher',
+            'Práctica lo aprendido en clases',
+            'No subas las tareas a las 11.59 XD',
+            'Si no te quedó muy claro un tema, busca tutoríales',
+            'Si terminaste una actividad en clase, ayuda a los que no entienden',
+            'Siempre escala la milla extra'
+        ];
+    
+        items.forEach(consejo => {
+            let li = document.createElement('li')
+            li.textContent = consejo;
+            ul.appendChild(li);
+        });
+    
+        div.appendChild(ul);
+        document.body.appendChild(div);
